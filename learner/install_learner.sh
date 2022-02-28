bba686c20c09a10e505fa77d41e25e3c6a267fb8
Giulia Milan <giuliapuntoit96@gmail.com>
2022-02-28 14:22:06 +0100

    Change pip to pip3

--- learner/install_learner.sh
+++ learner/install_learner.sh
@@ -21,11 +21,11 @@ cd cowrie
 git checkout $COWRIE 2>&1>/dev/null
 cd -
 
-pip install virtualenv
+pip3 install virtualenv
 virtualenv --python=python3 cannypot-env
 source cannypot-env/bin/activate
-pip install --upgrade pip
-pip install --upgrade -r $DIR/requirements.txt
+pip3 install --upgrade pip
+pip3 install --upgrade -r $DIR/requirements.txt
 
 # Copy customized cowrie files as well as the learning
 cp -R $DIR/* $PREFIX/cowrie/
