91595e0c04b058a537e1bbb9b9997db700f1f1d9
Giulia Milan <giuliapuntoit96@gmail.com>
2022-07-13 11:13:48 +0200

    Revert "Align cannypot to public version"
    This reverts commit 526bfe9e97de31b08fb09878727b7a4b789cef80.

--- learner/src/cowrie/learning/test/test_load_save_qTable.py
+++
@@ -1,6 +0,0 @@
-from learner.src.cowrie.learning.rl.learning_algorithm import CentralAlgorithm
-
-alg = CentralAlgorithm('Q-Learning', 0.1, 0.9, 0.5, 0.2, 50)
-alg.saveLearningState('q_table')
-alg.loadLearningState('q_table')
-print(alg.command_dict.outputs)
